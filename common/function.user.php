85af38570e13670e5252abed631c3ae209343cd2
ismartkorea <ismartkorea@hotmail.com>
2014-06-12 06:06:08 +0900

    commit;

--- common/function.user.php
+++ common/function.user.php
@@ -26,27 +26,27 @@ function popup_msg($msg) {
 	</script>");
 }
 
-##### ���� �߻�� �����ڵ带 ���ڷ� ��޹޾� ���� ��Ȳ�� �ش��ϴ� �޽����� �Բ� popup_msg() �Լ��� ȣ���Ѵ�.
+##### 에러발생시. 에러코드를 인자로 전달받아 에러 상황에 해당하는 메시지와 함께 popup_msg() 함수를 호출한다.
 function error($errcode) {
 	switch ($errcode) {
 		case ("NOT_FOUND_CONFIG_FILE") :
-			popup_msg("���� ���丮�� ������ ȯ�漳�� ������ ����ϴ�.");
+			popup_msg("현재 디렉토리에 참조할 환경설정 파일이 없습니다.");
 			break;
 
 		case ("ACCESS_DENIED_DB_CONNECTION") :
-			popup_msg("�����ͺ��̽� ���ῡ �����Ͽ��ϴ�.\\n\\n�����ϰ��� �ϴ� ������� ����ڸ�, ��й�ȣ�� Ȯ���Ͻñ� �ٶ�ϴ�.");
+			popup_msg("데이터베이스 연결에 실패하였습니다.\\n\\n연결하고자 하는 서버명과 사용자명, 비밀번호를 확인하시길 바랍니다.");
 			break;
 			
 		case ("FAILED_TO_SELECT_DB") :
-			popup_msg("������ �����ͺ��̽��� �۾���� �����ͺ��̽��� �� �� ����ϴ�.\\n\\n������ �����ͺ��̽��� Ȯ���Ͻñ� �ٶ�ϴ�.");
+			popup_msg("지정한 데이터베이스를 작업대상 데이터베이스로 할 수 없습니다.\\n\\n지정한 데이터베이스를 확인하시길 바랍니다.");
 			break;
 			
 		case ("LOGIN_IN_NOT_FOUND") :
-			popup_msg("��ġ�ϴ� ȸ�������� ����ϴ�.\\n\\n�ٽ� Ȯ���Ͻð� �α����ϼ���.");
+			popup_msg("입력하신 로그인 아이디를 찾을 수가 없습니다.");
 			break;
 			
 		case ("LOGIN_INVALID_PW") :
-			popup_msg("��й�ȣ�� Ʋ���ϴ�.\\n\\n��� ��ȣ�� Ȯ���ϼ���.");
+			popup_msg("입력하신 로그인 패스워드가 다릅니다.");
 			break;
 			
 		case ("QUERY_ERROR") :
